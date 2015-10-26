f7741ba41ce34875b34c5268081ba273e001bdbb
Matth_eu <matthew@matth.eu>
2015-10-26 23:02:02 +0000

    field link

---
+++ assets/js/src/views/field-link.js
@@ -0,0 +1,33 @@
+var $ = require('jquery');
+var Field = require('views/field');
+
+var FieldLink = Field.extend({
+
+	template:  $( '#tmpl-mpb-field-link' ).html(),
+
+	events: {
+		'keyup   input.field-text': 'textInputChanged',
+		'change  input.field-text': 'textInputChanged',
+		'keyup   input.field-link': 'linkInputChanged',
+		'change  input.field-link': 'linkInputChanged',
+	},
+
+	textInputChanged: _.debounce( function(e) {
+		if ( e && e.target ) {
+			var value = this.getValue();
+			value.text = e.target.value;
+			this.setValue( value );
+		}
+	} ),
+
+	linkInputChanged: _.debounce( function(e) {
+		if ( e && e.target ) {
+			var value = this.getValue();
+			value.link = e.target.value;
+			this.setValue( value );
+		}
+	} ),
+
+});
+
+module.exports = FieldLink;
