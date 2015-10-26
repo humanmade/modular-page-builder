b1f7e6b01df225621106f2d70ba463b7c92d6e1c
Matth_eu <matthew@matth.eu>
2015-10-26 23:01:29 +0000

    remove old modules

--- assets/js/src/utils/edit-view-map.js
+++ assets/js/src/utils/edit-view-map.js
@@ -3,10 +3,8 @@
  */
 var editViewMap = {
 	'header':              require('views/module-edit-header'),
-	'textarea':            require('views/module-edit-textarea'),
 	'text':                require('views/module-edit-text'),
 	'image':               require('views/module-edit-image'),
-	'video':               require('views/module-edit-video'),
 	'blockquote':          require('views/module-edit-blockquote'),
 };
 
