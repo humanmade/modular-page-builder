e150bc10648209a8eba9cc5366c02e0e5febfba5
Matth_eu <matthew@matth.eu>
2015-10-26 23:02:42 +0000

    field textarea

---
+++ assets/js/src/views/field-textarea.js
@@ -0,0 +1,15 @@
+var $ = require('jquery');
+var FieldText = require('views/field-text');
+
+var FieldTextarea = FieldText.extend({
+
+	template:  $( '#tmpl-mpb-field-textarea' ).html(),
+
+	events: {
+		'keyup  textarea': 'inputChanged',
+		'change textarea': 'inputChanged',
+	},
+
+} );
+
+module.exports = FieldTextarea;
