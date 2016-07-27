9aee471ff93c69f95ee599061755c71cdc85d3b9
Matth_eu <matthew@matth.eu>
2016-07-27 19:43:55 +0100

    Remove meta revisioning

--- inc/class-builder-post-meta.php
+++ inc/class-builder-post-meta.php
@@ -26,15 +26,6 @@ class Builder_Post_Meta extends Builder {
 			return $response;
 		}, 11, 2 );
 
-		add_filter( "wp_get_revision_ui_diff", array( $this, 'revision_ui_diff' ), 10, 3 );
-
-		add_filter( '_wp_post_revision_fields', function( $fields ) {
-			$fields['modular-page-builder-data'] = __( 'Modular Page Builder Data' );
-			$fields['modular-page-builder-nonce'] = __( 'Modular Page Builder Data' );
-			$fields['modular-page-builder-allowed-modules'] = __( 'Modular Page Builder Data' );
-			return $fields;
-		} );
-
 		add_action(
 			'admin_enqueue_scripts',
 			function() {
